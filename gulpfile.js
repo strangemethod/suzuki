64965fb465d96c716ff30abb522db5e5b5eec6a2
Andrew Bowles <Andrew.Bowles@rga.com>
2017-05-15 11:37:57 -0700

    update package.json and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	notify = require('gulp-notify'),
+	autoprefixer = require('gulp-autoprefixer'),
+	path = require('path');
+
+var paths = {
+	sass: './sass/',
+	scripts: './js/',
+};
+
+gulp.task('sass', function() {
+	return gulp.src(path.join(paths.sass, '/**/*.scss'))
+		.pipe(sass({ style: 'expanded', sourceComments: 'map', errLogToConsole: true}))
+		.pipe(autoprefixer('last 2 version', "> 1%", 'ie 8', 'ie 9'))
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('./public/css'))
+		.pipe(notify({ message: 'SASS compiled!' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(path.join(paths.scripts, '*.js'))
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./'))
+	.pipe(notify({ message: 'JS compiled!' }));
+});
+
+
+gulp.task('watch', function() {
+
+	gulp.watch(path.join(paths.sass, '/**/*.scss'), ['sass']);
+	gulp.watch(path.join(paths.scripts, '*.js'), ['scripts']);
+
+});
+
+
+gulp.task('default', ['sass', 'scripts', 'watch'], function() {
+
+});
